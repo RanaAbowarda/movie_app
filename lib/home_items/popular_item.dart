34bca08b18fa7db444bb1fa481204e5edf76210a
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-08-29 00:18:54 +0300

    Finish the top part of homeTab

--- lib/home_items/popular_item.dart
+++ lib/home_items/popular_item.dart
@@ -14,12 +14,15 @@ class PopularItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return BlocProvider(
       create: (context) => PopularViewModel()..getPopular(),
       child: BlocBuilder<PopularViewModel, PopularStates>(
 
           // bloc: PopularViewModel()..getPopular(),
           builder: (context, state) {
+        var resultList = BlocProvider.of<PopularViewModel>(context).resultList;
         if (state is PopularLoadingState) {
           return Center(
             child: CircularProgressIndicator(
@@ -44,24 +47,27 @@ class PopularItem extends StatelessWidget {
               children: [
                 Container(
                   width: double.infinity,
-                  decoration: const BoxDecoration(
+                  decoration: BoxDecoration(
                       image: DecorationImage(
                           // fit: BoxFit.fill,
-                          image: AssetImage("assets/images/background.png"))),
+                          image: NetworkImage(
+                              "https://image.tmdb.org/t/p/w500${resultList[itemIndex].backdropPath}"))),
                   child: Padding(
-                    padding: const EdgeInsets.only(bottom: 30.0, left: 148),
+                    padding: EdgeInsets.only(
+                        bottom: height * 0.01, left: width * 0.45),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.end,
                       crossAxisAlignment: CrossAxisAlignment.stretch,
                       children: [
                         Text(
-                          BlocProvider.of<PopularViewModel>(context)
-                              .resultList[1]
-                              .backdropPath!,
+                          resultList[itemIndex].title!,
                           style: Theme.of(context).textTheme.titleMedium,
                         ),
+                        SizedBox(
+                          height: 5,
+                        ),
                         Text(
-                          "2019  PG-13  2h 7m",
+                          resultList[itemIndex].releaseDate!,
                           style: Theme.of(context).textTheme.titleSmall,
                         ),
                       ],
@@ -70,8 +76,27 @@ class PopularItem extends StatelessWidget {
                 ),
                 Positioned(
                   top: 150,
-                  child: Image.asset(
-                    "assets/images/Image.png",
+                  left: 20,
+                  child: Container(
+                    width: width * 0.35,
+                    height: height * 0.25,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            "https://image.tmdb.org/t/p/w500${resultList[itemIndex].posterPath}",
+                          ),
+                          fit: BoxFit.cover,
+                        )),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Image.asset(
+                          'assets/images/bookmark.png',
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -79,7 +104,7 @@ class PopularItem extends StatelessWidget {
             options: CarouselOptions(
               height: 350,
               aspectRatio: 16 / 9,
-              viewportFraction: 0.9,
+              viewportFraction: 1,
               initialPage: 0,
               enableInfiniteScroll: true,
               reverse: false,
